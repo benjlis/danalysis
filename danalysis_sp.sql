1c42d0b66b53daf3f67f2b2ae82f48f8190a4a83
Ben Lis <ben@leismart.com>
2018-11-13 15:00:15 -0500

    Added danalysis_dysel(), update of row_count and table found/not found logic.

--- danalysis_sp.sql
+++ danalysis_sp.sql
@@ -1,3 +1,13 @@
+create or replace function danalysis_dysel(qexpr text)
+returns text as $$
+declare
+    qresult text;
+begin
+    execute 'select ' || qexpr into strict qresult;
+    return qresult;
+end $$
+language plpgsql;
+
 create or replace function danalysis(
     table_name_arg    information_schema.sql_identifier,
     table_schema_arg  information_schema.sql_identifier = current_schema()
@@ -5,19 +15,33 @@ create or replace function danalysis(
 returns record as $$
 declare
     ret record;
+    rcnt int;
 begin
-   delete from danalysis where table_schema = table_schema_arg and
-                               table_name = table_name_arg;
+    delete from danalysis
+       where table_schema = table_schema_arg and
+             table_name = table_name_arg;
+   -- WHy not use upsert instead of delete/insert? Delete/insert enables a
+   -- call to danalysis to clear stats for dropped tables.
    insert into danalysis (table_schema, table_name, danalysis_time, row_count)
    select table_schema, table_name, current_timestamp, 0
       from information_schema.tables
       where table_schema = table_schema_arg and
             table_name = table_name_arg;
-    select table_schema, table_name, danalysis_time, row_count
-      into ret
-      from danalysis
-      where table_schema = table_schema_arg and
-            table_name = table_name_arg;
+   get diagnostics rcnt = ROw_COUNT;
+   if rcnt = 1 then
+      update danalysis
+         set row_count =
+            cast(danalysis_dysel('count(*) from '|| table_name_arg) as integer)
+         where table_schema = table_schema_arg and
+               table_name = table_name_arg;
+      select table_schema, table_name, danalysis_time, row_count
+         into ret
+         from danalysis
+         where table_schema = table_schema_arg and
+               table_name = table_name_arg;
+    else
+        select 'Table not found' into ret;
+    end if;
     return ret;
 end $$
 language plpgsql;
