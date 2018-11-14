9c411fd3243635466a37a4d34c46730c24e3ce84
Ben Lis <ben@leismart.com>
2018-11-14 15:28:12 -0500

    Added column level processing.

--- danalysis_sp.sql
+++ danalysis_sp.sql
@@ -22,13 +22,14 @@ begin
              table_name = table_name_arg;
    -- WHy not use upsert instead of delete/insert? Delete/insert enables a
    -- call to danalysis to clear stats for dropped tables.
+   -- Add table level details.
    insert into danalysis (table_schema, table_name, danalysis_time)
    select table_schema, table_name, current_timestamp
       from information_schema.tables
       where table_schema = table_schema_arg and
             table_name = table_name_arg;
    get diagnostics rcnt = ROw_COUNT;
-   if rcnt = 1 then
+   if rcnt = 1 then   -- table exists
       update danalysis
          set row_count = cast(danalysis_dysel('count(*) from '||
                             table_name_arg) as integer),
@@ -38,12 +39,28 @@ begin
                                      table_name = table_name_arg)
          where table_schema = table_schema_arg and
                table_name = table_name_arg;
+      -- column level processing
+      insert into danalysis_columns (table_schema, table_name,
+         column_name, ordinal_position)
+      select table_schema, table_name, column_name, ordinal_position
+         from information_schema.columns
+         where table_schema = table_schema_arg and
+               table_name = table_name_arg;
+      update danalysis_columns
+         set count_not_null = cast(danalysis_dysel('count(' || column_name ||
+                                 ') from '|| table_name) as integer),
+             count_distinct = cast(danalysis_dysel('count(distinct ' ||
+                                 column_name || ') from '|| table_name)
+                                 as integer)
+        where table_schema = table_schema_arg and
+              table_name = table_name_arg;
+      --
       select table_schema, table_name, danalysis_time, row_count, column_count
          into ret
          from danalysis
          where table_schema = table_schema_arg and
                table_name = table_name_arg;
-    else
+    else  --table does not exist
         select 'Table not found' into ret;
     end if;
     return ret;
